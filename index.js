101f0a93a347f32eb4233cf98915d8f730ade4b1
wjlai1234 <72342994+wjlai1234@users.noreply.github.com>
2022-09-28 23:09:53 +0800

    init

--- index.js
+++ index.js
@@ -43,16 +43,16 @@ app.get("/search", (req, res) => {
   request(
     "https://jsonplaceholder.typicode.com/comments",
     function (error, response, body) {
-    console.error("query:", req.query); // Print the error if one occurred
-    console.error("error:", error); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-      console.log("body:", JSON.parse(body)[0]); // Print the response status code if a response was received
+      // console.error("query:", req.query); // Print the error if one occurred
+      // console.error("error:", error); // Print the error if one occurred
+      //   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      //   console.log("body:", JSON.parse(body)[0]); // Print the response status code if a response was received
 
       const filters = req.query;
       const filteredUsers = JSON.parse(body).filter((comment) => {
         let isValid = true;
         for (var key in filters) {
-          console.log(key, comment[key], filters[key]);
+          //   console.log(key, comment[key], filters[key]);
           isValid = isValid && comment[key] == filters[key];
         }
         return isValid;
